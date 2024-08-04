42c2b83cd8b30cbabfd2c29e66401b431d80b4e8
JrSergio00 <junior.sergio@escolar.ifrn.edu.br>
2024-08-04 17:43:15 -0300

    Mudanças na section inicial e maior adequação à paleta de cores da empresa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,35 @@
+let randomNum
+let videoElement = document.getElementById('myVideo')
+
 document.addEventListener("DOMContentLoaded", function () {
     changeVideoSrc()
 })
 
-function getRandomVideoSrc() {
-    const randomNum = Math.floor(Math.random() * 9) + 1;
-    return `assets/img/v${randomNum}.mp4`;
+function getRandomVideoSrc(randomNum) {
+    return `assets/img/v${randomNum}.mp4`
 }
 
 function changeVideoSrc() {
-    const videoElement = document.getElementById('myVideo');
-    videoElement.src = getRandomVideoSrc();
-    videoElement.load();
+    randomNum = Math.floor(Math.random() * 9) + 1
+    videoElement.src = getRandomVideoSrc(randomNum)
+    videoElement.load()
+}
+
+function previousVideo() {
+    if(randomNum == 1) {
+        randomNum = 9
+    } else {
+        randomNum = --randomNum
+    }
+    videoElement.src = getRandomVideoSrc(randomNum)
+    videoElement.load()
+}
+function nextVideo() {
+    if(randomNum == 9) {
+        randomNum = 1
+    } else {
+        randomNum = ++randomNum
+    }
+    videoElement.src = getRandomVideoSrc(randomNum)
+    videoElement.load()
 }
